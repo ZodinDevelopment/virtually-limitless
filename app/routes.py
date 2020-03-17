59756405f8e4753a94f61c9b8b3f32e11421259f
toasterkief <toasterkief@localhost.localdomain>
2020-03-17 12:58:08 -0500

    login form and view

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask import flash, render_template, redirect, url_for, request
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 def index():
@@ -22,3 +24,10 @@ def index():
         }
     ]
     return render_template("index.html", title="Dashboard", user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    # this is like the function above but will handle more data and has a FlaskForm initialized
+    form = LoginForm()
+    return render_template('login.html')
