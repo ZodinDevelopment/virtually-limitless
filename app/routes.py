3b70c465de359bf660e037f5ece3ec593e34d86e
toasterkief <toasterkief@localhost.localdomain>
2020-03-17 12:38:26 -0500

    App route to template view, sample dynamic content

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
+from datetime import datetime
+from flask import flash, render_template, redirect, url_for, request
 from app import app
 
 @app.route('/')
 def index():
-    return "In development"
+    # This is a sample user for testing templates
+    user = {'username': 'Toaster'}
 
+    posts = [
+        {
+            "author": {"username": "admin"},
+            "headline": "Good news everyone!",
+            "date_time_posted": datetime.now().ctime(),
+            "body": "Hey, make sure you're running the latest version of Python!"
+        },
+        {
+            "author": {"username": "Trey"},
+            "headline": "How is everyone?!",
+            "date_time_posted": datetime.now().ctime(),
+            "body": "Don't waste your brilliance on the streets! Come train with the Jedi!"
+        }
+    ]
+    return render_template("index.html", title="Dashboard", user=user, posts=posts)
